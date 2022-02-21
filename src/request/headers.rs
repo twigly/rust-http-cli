12bebeb84b603a80fcfe6d72f07891a3d87968e2
twigly <96623225+twigly@users.noreply.github.com>
2022-02-21 18:52:37 +0000

    fmt

--- src/request/headers.rs
+++ src/request/headers.rs
@@ -1,4 +1,4 @@
-use crate::core::{WorkspaceData, Workspace};
+use crate::core::{Workspace, WorkspaceData};
 use crate::{rh_homepage, rh_name, rh_version};
 use reqwest::header::{HeaderMap, HeaderValue};
 
@@ -7,35 +7,20 @@ use super::header;
 pub fn upgrade(args: &Workspace, headers: &mut HeaderMap) {
     if args.is_json() {
         if !headers.contains_key(header::CONTENT_TYPE) {
-            headers.append(
-                header::CONTENT_TYPE,
-                HeaderValue::from_str("application/json").unwrap(),
-            );
+            headers.append(header::CONTENT_TYPE, HeaderValue::from_str("application/json").unwrap());
         }
         if !headers.contains_key(header::ACCEPT) {
-            headers.append(
-                header::ACCEPT,
-                HeaderValue::from_str("application/json").unwrap(),
-            );
+            headers.append(header::ACCEPT, HeaderValue::from_str("application/json").unwrap());
         }
     }
     if args.is_form() && !headers.contains_key(header::CONTENT_TYPE) {
-        headers.append(
-            header::CONTENT_TYPE,
-            HeaderValue::from_str("application/x-www-form-urlencoded").unwrap(),
-        );
+        headers.append(header::CONTENT_TYPE, HeaderValue::from_str("application/x-www-form-urlencoded").unwrap());
     }
 
     if !headers.contains_key(header::USER_AGENT) {
         headers.append(
             header::USER_AGENT,
-            HeaderValue::from_str(&format!(
-                "{}/{} {}",
-                rh_name!(),
-                rh_version!(),
-                rh_homepage!(),
-            ))
-            .unwrap(),
+            HeaderValue::from_str(&format!("{}/{} {}", rh_name!(), rh_version!(), rh_homepage!(),)).unwrap(),
         );
     }
 }
